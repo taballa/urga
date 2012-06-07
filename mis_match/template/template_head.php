17eb7eaf3ed16f9fd0970486480ccdd4824a01ec
taballa <taballa@gmail.com>
2012-06-07 19:27:49 +0800

    121

--- mis_match/template/template_head.php
+++ mis_match/template/template_head.php
@@ -1,8 +1,20 @@
-<!DOCTYPE HTML>
-<html lang="zh-CN">
-<head>
-	<meta charset="UTF-8">
-	<title>Title</title>
-	<link rel="stylesheet" type="text/css" href="../style/main.css" media="all">
-	<link rel="stylesheet" type="text/css" href="../style/page/index.css" media="all">
-</head>
+<?php
+	fonction output_head($page_name){
+		if (!$page_name){
+		?>
+			<!DOCTYPE HTML>
+			<html lang="zh-CN">
+			<head>
+				<meta charset="UTF-8">
+				<title>Title</title>
+				<link rel="stylesheet" type="text/css" href="../style/main.css" media="all">
+				<link rel="stylesheet" type="text/css" href="../style/page/index.css" media="all">
+			    <!--[if lte IE 7]>
+			        <link rel="stylesheet" href="../style/iehacks.css" type="text/css"/>
+			    <![endif]-->
+			</head>
+		<?php
+		}
+		
+	}
+?>
